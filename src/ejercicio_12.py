b709e699225f03f8e83321f8d0ae58f557059357
johan galvis <johangalvis092@gmail.com>
2025-08-05 15:03:21 -0500

    act 12

---
+++ src/ejercicio_12.py
@@ -0,0 +1,9 @@
+n = int(input("Introduce un número entero: "))
+factorial = 1
+contador = 1
+
+while contador <= n:
+    factorial *= contador
+    contador += 1
+
+print(f"El factorial de {n} es: {factorial}")
