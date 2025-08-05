06cbc3b26c9ff209163fc816d1871a8f59bdfbc8
johan galvis <johangalvis092@gmail.com>
2025-08-05 14:55:04 -0500

    act 4

---
+++ src/ejercicio_04.py
@@ -0,0 +1,8 @@
+suma = 0
+numero = None
+
+while numero != 0:
+    numero = int(input("Ingresa un número (0 para terminar): "))
+    suma += numero
+
+print("La suma total de los números ingresados es:", suma)
